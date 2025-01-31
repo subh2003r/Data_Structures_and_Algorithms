2e84fcdc6be24b4dab5a9f03f8316d1aa736120b
Subhankar Sah <subhankarsah95@gmail.com>
2025-01-31 22:22:47 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (15.98%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,7 +1,13 @@
 # 1) first approach -> solve using power set 
 # check whether ith bit is set, if it is set then include its corresponding element else not 
 
+# 2) second approach -> solve using recursion
+
 class Solution:
+    def __init__(self):
+        self.store = []
+
+    '''
     def subsets(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
         m = 2**n
@@ -16,3 +22,26 @@ class Solution:
             store.append(subSeq)
 
         return store
+        '''
+
+    def recur(self,nums,index,subSeq):
+        '''
+        Why does subSeq[:] fix the issue?
+    subSeq[:] creates a shallow copy of the list.
+    The copy is stored in self.store, so future modifications to subSeq do not affect       previously stored subsets.
+
+        '''
+        if index == len(nums):
+            self.store.append(subSeq[:]) # append a copy of subSeq
+            return
+        
+        # pick the element
+        subSeq.append(nums[index])
+        self.recur(nums,index+1,subSeq)
+        subSeq.pop() # backtrack
+        # do not pick the element
+        self.recur(nums,index+1,subSeq)
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        self.recur(nums,0,[])
+        return self.store
