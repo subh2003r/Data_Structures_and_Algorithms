5b984ac150e3d7aa576aa27441abff77a3f0e6b8
Subhankar Sah <subhankarsah95@gmail.com>
2025-02-04 11:06:25 +0530

    Time: 7 ms (97.82%) | Memory: 18.1 MB (80.89%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,6 +1,8 @@
 class Solution:
     def __init__(self):
         self.result = []
+    '''
+    # The isPossible() function takes O(3n) time to check
 
     def isPossible(self,board,row,col,n):
         # checking the above columns
@@ -25,19 +27,31 @@ class Solution:
             new_col += 1
 
         return True
+        '''
 
-    def findWays(self,board,n,row):
+    def findWays(self,board,n,row,rightDiag,leftDiag,cols):
         if row == n:
             self.result.append(["".join(val) for val in board])
             return
 
         for col in range(n):
-            if self.isPossible(board,row,col,n):
-                board[row][col] = 'Q' # safe configuration to place the queen 
-                self.findWays(board,n,row+1)
-                board[row][col] = '.'  # backtrack
+            # if self.isPossible(board,row,col,n):
+            if cols[col] or rightDiag[row+col] or leftDiag[n-1 + row-col]:
+                continue # skip invalid positions
+            cols[col] = True
+            rightDiag[row+col] = True
+            leftDiag[n-1 + row-col] = True
+            board[row][col] = 'Q' # safe configuration to place the queen 
+            self.findWays(board,n,row+1,rightDiag,leftDiag,cols)
+            cols[col] = False
+            rightDiag[row+col] = False
+            leftDiag[n-1 + row-col] = False
+            board[row][col] = '.'  # backtrack
 
     def solveNQueens(self, n: int) -> List[List[str]]:
         board = [['.']*n for _ in range(n)]
-        self.findWays(board,n,0)
+        cols = [False]*n
+        rightDiag = [False] * (2*n-1) # upper right diagonal
+        leftDiag = [False] * (2*n-1) # upper left diagonal
+        self.findWays(board,n,0,rightDiag,leftDiag,cols)
         return self.result
