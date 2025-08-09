ca0015716adfa470e0990eef65b0b4e34a6ab0c1
Subhankar Sah <subhankarsah95@gmail.com>
2025-08-10 00:55:26 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (58.48%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,11 +1,14 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
+        '''
+        # Brute force approach 
+
         res = [[1]]
         
         if numRows == 1:
             return res
         numRows -= 1
-        
+
         while numRows:
             num_res = res[-1]
             interim_res = [1]
@@ -18,3 +21,43 @@ class Solution:
             numRows -= 1
 
         return res
+        '''
+
+        '''
+        # this is similar to combination 
+        nCr = nC0 + nC1 + nC2 + .... + nCr-1
+        # Given n = 5, find the r=3 term
+
+        def findNcr(n,r):
+            res = 1
+            for i in range(r):
+                res *= (n-i)
+                res //= (i+1)
+            
+            return res
+
+        # Find n = 5 pascals value
+        
+        res = 1
+        final_value = [1]
+
+        for r in range(1,n):
+            res *= (n-r)
+            res //= r
+            
+            fina_value.append(res)
+        '''
+
+        final_result = []
+
+        for row in range(1, numRows+1):
+            interim_result = [1]
+            ans = 1
+            for col in range(1, row):
+                ans *= (row-col)
+                ans //= col
+                interim_result.append(ans)      
+                  
+            final_result.append(interim_result)
+
+        return final_result
