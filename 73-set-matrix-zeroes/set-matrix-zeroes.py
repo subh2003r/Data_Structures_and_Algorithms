23e476fdbb17f30e44f8470a82dd1470fc16bb58
Subhankar Sah <subhankarsah95@gmail.com>
2025-08-09 20:47:37 +0530

    Time: 7 ms (20.18%) | Memory: 18.4 MB (78.97%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -25,18 +25,26 @@ class Solution:
         col0 = 1 # to keep track of col0 as it was overallaped i.e handling edge cases
 
         for i in range(m):
-            if matrix[i][0] == 0:
-                col0 = 0
-            for j in range(1, n):
+            for j in range(n):
                 if matrix[i][j] == 0:
                     matrix[i][0] = 0
-                    matrix[0][j] = 0
+                    if j != 0:
+                        matrix[0][j] = 0
+                    else:
+                        col0 = 0
 
-    # Step 2: Fill zeros in reverse order to protect markers
-        for i in range(m - 1, -1, -1):
-            for j in range(n - 1, 0, -1):
-                if matrix[i][0] == 0 or matrix[0][j] == 0:
-                    matrix[i][j] = 0
-            if col0 == 0:
+
+        for i in range(1,m):
+            for j in range(1,n):
+                if matrix[i][j] != 0:
+                    if (matrix[0][j] == 0 or matrix[i][0] == 0):
+                        matrix[i][j] = 0
+
+
+        if matrix[0][0] == 0:
+            for j in range(0,n):
+                matrix[0][j] = 0
+
+        if col0 == 0:
+            for i in range(0,m):
                 matrix[i][0] = 0
-
