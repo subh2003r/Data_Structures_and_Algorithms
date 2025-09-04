4eec1eaa7a6770822833ddc0a02164f9758900df
Subhankar Sah <subhankarsah95@gmail.com>
2025-09-04 10:55:56 +0530

    Time: 13 ms (49.39%) | Memory: 17.9 MB (50.17%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        def check():
+            store_cnt = {}
+            for char in s:
+                store_cnt[char] = store_cnt.get(char,0) + 1
+            
+            for char in t:
+                if char in store_cnt and store_cnt[char] > 0:
+                    store_cnt[char] -= 1
+                else:
+                    return False
+
+            return True if sum(store_cnt.values()) == 0 else False
+
+        return len(s) == len(t) and check()
