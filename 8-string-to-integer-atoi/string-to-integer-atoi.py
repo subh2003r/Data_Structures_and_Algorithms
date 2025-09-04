1ce086990f0b743ca7a410e5ab7c684cb759e07c
Subhankar Sah <subhankarsah95@gmail.com>
2025-09-04 23:08:58 +0530

    Time: 2 ms (45.51%) | Memory: 17.6 MB (95.33%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,6 +1,7 @@
 # subhankar
 class Solution:
     def myAtoi(self, s: str) -> int:
+        '''
         s = s.lstrip() # removing leading white spaces
         if not s:
             return 0
@@ -27,3 +28,34 @@ class Solution:
             return conversion(num,index+1)
         
         return conversion(0,0)
+        '''
+
+        n = len(s)
+        index = 0
+        res = 0
+        sign = 1
+        max_range, min_range = 2**31-1, -2**31
+
+        # skip leading spaces
+        while index < n and s[index] == ' ':
+            index += 1
+            
+        # check the sign
+        if index < n and (s[index] == '-' or s[index] == '+'):
+            sign = -1 if s[index] == '-' else 1
+            index += 1
+
+        '''
+        res * 10 + digit > max_range
+        res > (max_range - digit) // 10 
+        '''
+        while index < n and s[index].isdigit():
+            digit = int(s[index])
+            
+            if res > (max_range - digit) // 10:
+                return max_range if sign == 1 else min_range
+
+            res = res*10 + digit
+            index += 1
+
+        return sign*res
