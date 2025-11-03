53062798600a5012ebd2b0789ffca2a77febcfd2
Subhankar Sah <subhankarsah95@gmail.com>
2025-11-03 22:45:11 +0530

    Time: 12 ms (17.92%) | Memory: 21.6 MB (27.39%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -1,5 +1,9 @@
 from queue import LifoQueue
+"""
 class MinStack:
+    
+    # Time complexity: O(1)
+    # Space complexity: O(2N) as we are storing pairs
 
     def __init__(self):
         self.store = LifoQueue()
@@ -19,7 +23,40 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.store.queue[-1][1]
+"""
 
+class MinStack:
+    # Time complexity: O(1)
+    # Space complexity: O(N)
+    def __init__(self):
+        self.mini = float("inf")
+        self.store = LifoQueue()
+
+    def push(self, val: int) -> None:
+        if self.store.qsize() == 0:
+            self.mini = val
+            self.store.put(val)
+        else:
+            if val < self.mini:
+                self.store.put(2*val-self.mini)
+                self.mini = val
+            else:
+                self.store.put(val)
+
+    def pop(self) -> None:
+        if self.store.queue[-1] < self.mini:
+            self.mini = 2*self.mini - self.store.queue[-1]
+        
+        self.store.get()    
+
+    def top(self) -> int:
+        if self.store.queue[-1] < self.mini:
+            return self.mini
+
+        return self.store.queue[-1]
+
+    def getMin(self) -> int:
+        return self.mini
 
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
