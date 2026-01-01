f95c5b1e126af7b2b37049e1c984e415ede88362
Subhankar Sah <subhankarsah95@gmail.com>
2026-01-01 21:20:27 +0530

    Time: 11 ms (23.98%) | Memory: 22.7 MB (23.68%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,8 +1,36 @@
 from collections import Counter
 import heapq
+"""
+Using minheap
+# Space complexity: O(k)
+# Time complexity: O(n) + O(nlogk)
+"""
 
+"""
+Using bucket sort
+# Space complexity: O(n)
+# Time complexity: O(n)
+"""
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # using bucket sort
+        result = []
+        freq = Counter(nums)
+        bucket = [[] for _ in range(len(nums)+1)]
+        
+        for key, count in freq.items():
+            bucket[count].append(key)
+
+        for i in range(len(bucket)-1, -1, -1):
+            for key in bucket[i]:
+                result.append(key)
+                if len(result) == k:
+                    return result
+        
+        return result
+
+        """
+        # minheap
         freq = Counter(nums)
         heap = []
         
@@ -12,4 +40,5 @@ class Solution:
                 heapq.heappop(heap)
         
         return [key for _, key in heap] 
+        """
         
