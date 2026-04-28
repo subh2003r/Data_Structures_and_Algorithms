bf088078dba526fa1229d3416d7505e63533a71d
Subhankar Sah <subhankarsah95@gmail.com>
2026-04-28 23:33:17 +0530

    Time: 3 ms (87.49%) | Memory: 20.4 MB (65.42%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,27 +1,58 @@
+"""
+Topological sort is an ordering of nodes in directed graph such that:
+for every directed edge u -> v, 'u' comes before 'v' in the ordering
+
+Topological sort is only possible in "Directed Acyclic Graph (DAG)"
+    If there is a cycle, no valid ordering exists
+
+    Intuition:
+    Think of task as an dependencies 
+    'Do A before B'
+    'Do B before C'
+
+    Ordering looks like -> 'A -- B -- C'
+"""
+
+"""
+Kahn's algorithm
+
+1. Find out nodes with indegree 0 (nodes having no dependency should come first)
+2. push all indegree 0 nodes into queue
+3. iterate through the neighbours of node, and decrement indegree by 1 (this is done because current node dependency is done and since it is done, the neighbour dependency by 1 gets reduced)
+4. Check for cycle in the graph, if "yes" then no ordering is formed
+
+"""
+
+from collections import deque
+
 class Solution:
-    def canFinish(self, n: int, prerequisites: List[List[int]]) -> bool:
-        adj = [[] for _ in range(n)]
-        indegree = [0] * n
-        ans = []
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0]*numCourses
+        completedTask = 0
+        dq = deque()
 
-        for pair in prerequisites:
-            course = pair[0]
-            prerequisite = pair[1]
-            adj[prerequisite].append(course)
-            indegree[course] += 1
+        for u,v in prerequisites:
+            graph[v].append(u)
+            indegree[u] += 1
+        
+        # append indegree as 0 -> task with no dependency should be completed first 
+        for node in range(numCourses):
+            if indegree[node] == 0:
+                dq.append(node)
 
-        queue = deque()
-        for i in range(n):
-            if indegree[i] == 0:
-                queue.append(i)
+        while dq:
+            task = dq.popleft()
+            completedTask += 1
 
-        while queue:
-            current = queue.popleft()
-            ans.append(current)
+            for neighb in graph[task]:
+                indegree[neighb] -= 1
+                if indegree[neighb] == 0:
+                    # all dependency completed, now we can proceed to complete this current task -- so getting appended in queue
+                    dq.append(neighb)
+        
+        return True if completedTask == numCourses else False
 
-            for next_course in adj[current]:
-                indegree[next_course] -= 1
-                if indegree[next_course] == 0:
-                    queue.append(next_course)
 
-        return len(ans) == n
+
+                
