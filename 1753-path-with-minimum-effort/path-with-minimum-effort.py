b99a5de1f2cfe2c39cbe3a0ce57912c4b5a3b04e
Subhankar Sah <subhankarsah95@gmail.com>
2026-05-02 12:43:11 +0530

    Time: 457 ms (19.22%) | Memory: 20.7 MB (88.06%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.py
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.py
@@ -1,7 +1,13 @@
 from collections import deque
+import heapq as hq 
 
 class Solution:
-    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+    def simple_bfs(self, heights):
+        """
+        Normal BFS approach 
+        Only issue is the node processing order --> BFS processes in FIFO order 
+        Nodes can be visited multiple times .. thus increasing time complexity
+        """
         m,n = len(heights), len(heights[0])
         # create dist 2D array with n cols and m rows 
         dist = [[float("inf")]*n for _ in range(m)]
@@ -28,3 +34,38 @@ class Solution:
 
         return dist[m-1][n-1]
             
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        """
+        Using dijkstra algo, here it will gurantee minimum effort across path to reach the destination. Because at a time, dijkstra algo, will consider those path which req minimum effort.
+        """
+
+        m,n = len(heights), len(heights[0])
+        # create dist 2D array with n cols and m rows 
+        dist = [[float("inf")]*n for _ in range(m)]
+        directions = [(-1,0),(0,-1),(1,0),(0,1)]
+
+        dist[0][0] = 0
+
+        pq = [(0,0,0)] # row, col, effort 
+
+        while pq:
+            row, col, prev_effort = hq.heappop(pq)
+            # remove redundant cells
+            if prev_effort > dist[row][col]:
+                continue
+            
+            # dijkstra gurantees reaching destination with minimum effort as it is greedy in choosing the minimum effort path 
+            if row == m-1 and col == n-1:
+                return dist[row][col]
+            
+            for dr, dc in directions:
+                nr, nc = row+dr, col+dc
+                if 0 <= nr < m and 0 <= nc < n:
+                    curr_effort = abs(heights[nr][nc] - heights[row][col])
+                    new_effort = max(prev_effort, curr_effort)
+
+                    if new_effort < dist[nr][nc]:
+                        dist[nr][nc] = new_effort
+                        hq.heappush(pq, (nr,nc, new_effort))
+
+        return 0
