07dd3f59e0383b319ab8da47b668f4fa6b14d500
Subhankar Sah <subhankarsah95@gmail.com>
2026-07-12 16:40:59 +0530

    Time: 47 ms (61.95%) | Memory: 22.8 MB (25.00%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.py
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.py
@@ -5,8 +5,30 @@ class Solution:
         n = len(matrix)
         m = len(matrix[0])
 
+        def tabulation():
+            dp = [[0]*m for _ in range(n)]
+            res = 0
+
+            for i in range(n):
+                for j in range(m):
+                    if matrix[i][j] == 1:
+                        if i == 0 or j == 0:
+                            dp[i][j] = 1
+                        else:
+                            dp[i][j] = 1 + min(
+                                dp[i-1][j],
+                                dp[i][j-1],
+                                dp[i-1][j-1]
+                            )
+                        
+                        res += dp[i][j]
+                
+            return res           
+
+        return tabulation()
+
         @lru_cache(None)
-        def brute(i, j):
+        def memo(i, j):
             if i < 0 or j < 0:
                 return 0
             
@@ -16,9 +38,9 @@ class Solution:
             if i == 0 or j == 0:
                 return 1
             
-            left = brute(i, j-1)
-            top = brute(i-1, j)
-            diag = brute(i-1, j-1)
+            left = memo(i, j-1)
+            top = memo(i-1, j)
+            diag = memo(i-1, j-1)
 
             return 1 + min(left, top, diag)
         
@@ -27,7 +49,7 @@ class Solution:
         for i in range(n):
             for j in range(m):
                 if matrix[i][j] == 1:
-                    res += brute(i, j)
+                    res += memo(i, j)
 
         return res
         
