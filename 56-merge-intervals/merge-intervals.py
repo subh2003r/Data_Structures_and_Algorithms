b0d195c875630234c646ad81a6f6eb3e611bb8ea
Subhankar Sah <subhankarsah95@gmail.com>
2026-08-15 18:53:52 +0530

    Time: 7 ms (71.62%) | Memory: 23.3 MB (7.41%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,24 +1,36 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # intervals.sort(key=lambda x: x[0])
+        # merged = []
+        
+        # currStart, currEnd = intervals[0]
+        # # scan the intervals
+        # for start, end in intervals[1:]:
+        #     if start <= currEnd:
+        #         currStart = min(currStart, start)
+        #         currEnd = max(currEnd, end)
+        #     else:
+        #         merged.append([currStart, currEnd])
+        #         currStart = start
+        #         currEnd = end
+
+        # # append the last interval
+        # merged.append([currStart, currEnd])
+
+        # return merged
+
+        intervals.sort(key = lambda x: x[0])
+        currStart, currEnd = intervals[0]
         res = []
-        n = len(intervals)
 
-        # sorting the intervals
-        intervals.sort()
-
-        # brute force approach 
-        for i in range(0,n):
-            start, end = intervals[i][0], intervals[i][1]  
-
-            if res and res[-1][1] >= end:
-                continue
-
-            for j in range(i+1,n):
-                if intervals[j][0] <= end:
-                    end = max(end, intervals[j][1])
-                else:
-                    break
-
-            res.append([start,end])
-
+        for start, end in intervals[1:]:
+            if start <= currEnd:
+                currStart = min(currStart, start)
+                currEnd = max(currEnd, end)
+            else:
+                res.append([currStart, currEnd])
+                currStart = start
+                currEnd= end
+        
+        res.append([currStart, currEnd])
         return res
